2cc87bfe696a05cfbce40f6b53ef71365e0ed601
dina <dinaelhadidy966@gmail.com>
2023-09-25 16:30:13 -0400

    Document module

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
+"""Get info about employees using api"""
 import requests
 from sys import argv
 
-url = "https://jsonplaceholder.typicode.com/"
-emp_name = requests.get(url + "users/{}".format(argv[1])).json().get('name')
-todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    ename = requests.get(url + "users/{}".format(argv[1])).json().get('name')
+    todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
-completed = []
-for todo in todos:
-    if todo.get('completed'):
-        completed.append(todo.get('title'))
+    completed = []
+    for todo in todos:
+        if todo.get('completed'):
+            completed.append(todo.get('title'))
 
-print("Employee {} is done with tasks({}/{}):".format(
-    emp_name, len(completed), len(todos)))
-for todo in completed:
-    print("\t {}".format(todo))
+    print("Employee {} is done with tasks({}/{}):".format(
+        ename, len(completed), len(todos)))
+    for todo in completed:
+        print("\t {}".format(todo))
