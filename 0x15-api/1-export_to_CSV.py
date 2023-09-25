6181673f77240add699a7f71f5aa0ff5034b3280
dina <dinaelhadidy966@gmail.com>
2023-09-25 16:55:58 -0400

    Change name to username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,11 +6,12 @@ from sys import argv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    ename = requests.get(url + "users/{}".format(argv[1])).json().get('name')
+    ename = requests.get(url + "users/{}".format(argv[1]))
+    username = ename.json().get('username')
     todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
     with open('{}.csv'.format(argv[1]), 'w') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for todo in todos:
-            writer.writerow([argv[1], ename,
+            writer.writerow([argv[1], username,
                              todo.get('completed'), todo.get('title')])
