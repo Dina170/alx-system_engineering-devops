9ee9708a0c4469c7a513c14a11611f801ee2f510
dina <dinaelhadidy966@gmail.com>
2023-10-10 12:05:14 -0400

    Queries the Reddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import requests
+
+"""defines a function that queries the Reddit API"""
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API and returns the number of subscribers"""
+    headers = {"User-agent": "MyApp/1.0"}
+    res = requests.get("https://www.reddit.com/r/{}/about.json"
+                       .format(subreddit),
+                       headers=headers, allow_redirects=False)
+    if res.status_code != 200:
+        return 0
+    return res.json().get("data").get("subscribers")
